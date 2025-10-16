8105ea64392ac311132b2b63a0f794bd485ba4ff
Miika Alikirri <miika.alikirri@suse.com>
2025-10-16 11:29:46 +0300

    Basic display and fake edit of grub settings

--- src/app.tsx
+++ src/app.tsx
@@ -18,31 +18,187 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
-import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
+import { Page, PageSection, PageSectionVariants, PageSidebar, Form, TextInput, FormGroup, ActionGroup } from '@patternfly/react-core';
+import { Button } from '@patternfly/react-core';
+import { Modal, ModalHeader } from "@patternfly/react-core";
 
+import { useDialogs } from "dialogs.jsx";
+import { WithDialogs } from 'dialogs';
 import cockpit from 'cockpit';
+
+import { ListingTable } from 'cockpit-components-table.jsx';
 
 const _ = cockpit.gettext;
 
+/**
+ * @class
+ * @prop {number} line
+ * @prop {string} key
+ * @prop {string} value
+ */
+class KeyValue {
+    private original: string;
+    private line: number;
+    private changed: boolean;
+    public key: string;
+    public value: string;
+    /**
+     * Create a point.
+     * @prop {number} line - The x value.
+     * @prop {string} key - The x value.
+     * @prop {string} value - The y value.
+     */
+    constructor(original: string, line: number) {
+        this.original = original
+        this.line = line
+        this.changed = false
+        this.key = ''
+        this.value = ''
+        this.parse()
+    }
+
+    parse() {
+        // assuming this is always valid
+        // TODO: error out if the parse fails
+        // TODO: save the type of quotes so they can be returned to orignal
+        const trimmed = this.original.trim();
+        const keyval = trimmed.split('=')
+        this.key = keyval[0];
+        this.value = keyval[1].replace(/'|"/g, '');
+    }
+
+    toString() {
+        if (!this.changed) {
+            return this.original;
+        }
+
+        return `${this.key}="${this.value}"`;
+    }
+}
+
+class GrubFile {
+    private lines: (KeyValue | string)[]
+
+    constructor(data: string) {
+        this.lines = []
+        const lines = data.split('\n');
+        for (const idx in lines) {
+            const line = lines[idx]
+            const trimmed = line.trim();
+            if (trimmed.length === 0) {
+                this.lines.push(line);
+                continue;
+            }
+
+            if (trimmed[0] === '#') {
+                this.lines.push(line);
+                continue;
+            }
+
+            this.lines.push(new KeyValue(line, Number(idx)))
+        }
+    }
+
+    values() {
+        return this.lines.filter(value => (typeof value !== "string"))
+    }
+
+    toFile() {
+        return this.lines.map((val) => val.toString()).join('\n')
+    }
+}
+
+
+export const KeyValDialog = ({
+    name, value
+}: {
+    name: string,
+    value: string
+}) => {
+    const Dialogs = useDialogs();
+
+    return (
+        <Modal
+            title={_("Edit GRUB value")}
+            variant="small"
+            onClose={() => Dialogs.close()}
+            isOpen
+        >
+            <ModalHeader>
+                <Form>
+                    <FormGroup label={_("Key")} fieldId="key">
+                        <TextInput
+                            aria-label="Key"
+                            value={name}
+                            placeholder=""
+                        />
+                    </FormGroup>
+                    <FormGroup label={_("Value")} fieldId="value">
+                        <TextInput
+                            aria-label="Value"
+                            value={value}
+                            placeholder=""
+                        />
+                    </FormGroup>
+                    <ActionGroup>
+                        <Button variant="primary">
+                            {_("Save")}
+                        </Button>
+                        <Button variant="secondary">
+                            {_("Cancel")}
+                        </Button>
+                    </ActionGroup>
+                </Form>
+            </ModalHeader>
+        </Modal>
+    );
+};
+
+// Hack to hide the Sidebar area in patternfly 6 Page
+const emptySidebar = <PageSidebar isSidebarOpen={false} />;
+
+const GrubValues = ({ keyvals }: { keyvals: KeyValue[] }) => {
+    const Dialogs = useDialogs();
+
+    return (
+        <ListingTable
+            aria-label={_("GRUB values")}
+            gridBreakPoint='grid-lg'
+            columns={[
+                { title: _("Key") },
+                { title: _("Value") },
+            ]}
+            rows={keyvals.map((pkg, idx) => {
+                // const pkg = packages[key];
+                return {
+                    columns: [
+                        { title: pkg.key },
+                        { title: pkg.value },
+                        { title: <Button onClick={() => Dialogs.show(<KeyValDialog name={pkg.key} value={pkg.value} />)}>edit</Button> },
+                    ]
+                };
+            })}
+        />
+    )
+}
+
 export const Application = () => {
-    const [hostname, setHostname] = useState(_("Unknown"));
+    const [keyvals, setKeyvals] = useState<KeyValue[]>([]);
 
     useEffect(() => {
-        const hostname = cockpit.file('/etc/hostname');
-        hostname.watch(content => setHostname(content?.trim() ?? ""));
+        const hostname = cockpit.file('/etc/default/grub');
+
+        hostname.watch(content => setKeyvals(new GrubFile(content ?? "").values()));
         return hostname.close;
     }, []);
 
     return (
-        <Card>
-            <CardTitle>Starter Kit</CardTitle>
-            <CardBody>
-                <Alert
-                    variant="info"
-                    title={ cockpit.format(_("Running on $0"), hostname) }
-                />
-            </CardBody>
-        </Card>
+        <WithDialogs    >
+            <Page sidebar={emptySidebar} className='no-masthead-sidebar'>
+                <PageSection variant={PageSectionVariants.default} >
+                    <GrubValues keyvals={keyvals} />
+                </PageSection>
+            </Page>
+        </WithDialogs>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -17,14 +17,16 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
+
+import "cockpit-dark-theme";
+import 'patternfly/patternfly-6-cockpit.scss';
+import 'polyfills'; // once per application
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import "cockpit-dark-theme";
-
 import { Application } from './app.jsx';
 
-import "patternfly/patternfly-6-cockpit.scss";
 import './app.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
