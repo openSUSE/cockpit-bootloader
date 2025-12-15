c86188b54d873dd6f07255476138e6a83e5b864e
Miika Alikirri <miika.alikirri@suse.com>
2025-12-15 11:33:32 +0200

    Display which snapshot is selected

--- src/pages/snapshots.tsx
+++ src/pages/snapshots.tsx
@@ -10,6 +10,7 @@ import cockpit from "cockpit";
 
 import { ListingTable } from "cockpit-components-table.jsx";
 import {
+    BootkitSnapshots,
     Grub2SnapshotData,
     useBootKitContext,
 } from "../state/bootkit_provider";
@@ -53,21 +54,36 @@ const GrubConfig = ({ data }: { data: Grub2SnapshotData }) => {
 export const Snapshots = () => {
     const { snapshots } = useBootKitContext();
 
+    const isSelected = (snapData: BootkitSnapshots, idx: number) => {
+        // selected snapshot is either defined by selected_grub_id
+        // or selected snapshot is the last snapshot if selected_grub_id is not defined
+        if (snapData.selected.selected_grub_id) {
+            const selectedId = snapData.selected.selected_grub_id;
+            const snapshotId = snapData.snapshots[idx].snapshot.id;
+            return selectedId === snapshotId;
+        } else {
+            // The latest created snapshot is always the first in the list
+            return idx === 0;
+        }
+    };
+
     return (
         <ListingTable
             aria-label={_("GRUB snapshots")}
             gridBreakPoint="grid-lg"
             columns={[
                 { title: _("Id") },
+                { title: _("Is selected") },
                 { title: _("Config") },
                 { title: _("Selected kernel") },
                 { title: _("Created") },
             ]}
-            rows={snapshots.snapshots.map((data) => {
+            rows={snapshots.snapshots.map((data, idx) => {
                 const snapshot = data.snapshot;
                 return {
                     columns: [
                         { title: snapshot.id },
+                        { title: isSelected(snapshots, idx) ? _("Yes") : _("No") },
                         { title: snapshot.grub_config.slice(0, 20) },
                         { title: snapshot.selected_kernel },
                         { title: snapshot.created },
