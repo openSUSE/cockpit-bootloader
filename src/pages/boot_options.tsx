ef49f61e452f25633cc6a5892259038da1eb7f5a
Miika Alikirri <miika.alikirri@suse.com>
2026-06-15 10:59:42 +0300

    Add timeout field to Boot Options

--- src/pages/boot_options.tsx
+++ src/pages/boot_options.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Form, FormGroup, FormSelect, FormSelectOption } from '@patternfly/react-core';
+import { Form, FormGroup, FormSelect, FormSelectOption, NumberInput } from '@patternfly/react-core';
 import cockpit from 'cockpit';
 import { useBootKitContext } from '../state/bootkit_provider';
 
@@ -13,8 +13,35 @@ export const BootOptions = () => {
         updateConfig("boot_entries.selected", entry);
     };
 
+    const onTimeoutChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const value = (event.target as HTMLInputElement).value;
+        updateConfig("timeout", value);
+    };
+
+    const calcTimeout = (value: number) => {
+        let timeout = Number(config.timeout) || 0;
+        timeout += value;
+        if (timeout < 0) {
+            timeout = 0;
+        }
+
+        updateConfig("timeout", timeout.toString());
+    };
+
     return (
         <Form>
+            <FormGroup label={_("Timeout")} type="number" fieldId="bootloader-timeout">
+                <NumberInput
+                    value={Number(config.timeout) || 0}
+                    onPlus={() => calcTimeout(+1)}
+                    onMinus={() => calcTimeout(-1)}
+                    onChange={onTimeoutChange}
+                    inputName="bootloader-timeout-input"
+                    inputAriaLabel="Bootloader Timeout"
+                    minusBtnAriaLabel="minus"
+                    plusBtnAriaLabel="plus"
+                />
+            </FormGroup>
             <FormGroup label={_("Boot entry")} fieldId="graphical-console-resolution">
                 <FormSelect value={config.boot_entries.selected || config.boot_entries.boot_entries[0] || ""} onChange={(_event, value) => setEntry(value)}>
                     {config.boot_entries.boot_entries.map((value, idx) =>
